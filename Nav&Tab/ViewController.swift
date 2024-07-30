c6275469e39152e449c587cd29e97149fdae421f
NewSouthWave <tigers0110@naver.com>
2024-07-30 15:41:41 +0900

    Initial Commit

---
+++ Nav&Tab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nav&Tab
+//
+//  Created by Nam on 7/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
